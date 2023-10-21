1bc79d16717916d12355e6edb8a72dc540ae23af
sarah2244-4 <douthwaite.sarah@yahoo.co.uk>
2023-10-21 23:46:04 +0100

    Added mistake counter and random number generator

--- scripts/game.js
+++ scripts/game.js
@@ -1,33 +1,45 @@
 // global vairables
 
+const gridContainer = document.getElementById("sudoku-grid");
+const mistakeCounter = document.getElementById("counter");
+
 // functions
 
-// function to create board
-
-function generateBoard() {
-	// create 9x9 grid
-	// function to fill board with random numbers
-	// ensure board is valid and solvable
+// function to create 9x9 grid
+function createGrid() {
+	// Add rows and columns to empty html table 
+	// Fill with random numbers 1-9
+	Math.floor(Math.random() * 9) + 1;
+	// Check for repeated numbers in row
+	// Check for repeated numbers in column
+	// Check for repeated numbers in 3x3 grid
+	// Solve from top-left
 }
 
-// check answers are correct
+// Function to populate the Sudoku grid in the HTML table
 
+// Ensure board is valid and solvable
+
+// Check user inputs are correct
 function checkInput() {
-	// check validity of input
-	// count number of mistakes
+	// Check validity of user input
+	// If incorrect remove input from grid
+	// Count number of mistakes
 }
 
 // function to start new game on click
-
 function newGame() {
 	// check difficulty level
+	// fill in grid
+	createGrid();
 	// start time when new grid is loaded
+	// reset previous grid
 }
 
 // end time when puzzle is solved
 
 // change difficulty level
-
+// A given is a filled in tile
 function changeDiffultyLevel() {
 	// easy - 50 givens
 	// medium - 40 givens
@@ -35,8 +47,5 @@ function changeDiffultyLevel() {
 	// insane - 20 givens
 }
 
-// load how to play screen
-
+// Load how to play screen
 function displayGameRules() {}
-
-const sudokuGame = new SudokuGame();
