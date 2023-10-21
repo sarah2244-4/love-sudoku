92ad34af271b77f14a6d6cabe49fc3cc7ccba4ba
sarah2244-4 <douthwaite.sarah@yahoo.co.uk>
2023-10-21 22:54:02 +0100

    named js functions

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # love-sudoku
  Milestone project 2
+
+## User stories
+
+Users who access the application want to be able to: 
+
+* Play a game of sudoku
+* Know how long it has taken them 
+* Change difficulty depending on their level
+* Find out how to play if it is their first time playing
+* Start a new game while playing an existing game
+* Know how many wrong moves they have made
+* Have wrong moves removed from the board to assist them
+
+## UX design 
---
+++ scripts/game.js
@@ -0,0 +1,42 @@
+// global vairables
+
+// functions
+
+// function to create board
+
+function generateBoard() {
+	// create 9x9 grid
+	// function to fill board with random numbers
+	// ensure board is valid and solvable
+}
+
+// check answers are correct
+
+function checkInput() {
+	// check validity of input
+	// count number of mistakes
+}
+
+// function to start new game on click
+
+function newGame() {
+	// check difficulty level
+	// start time when new grid is loaded
+}
+
+// end time when puzzle is solved
+
+// change difficulty level
+
+function changeDiffultyLevel() {
+	// easy - 50 givens
+	// medium - 40 givens
+	// hard - 30 givens
+	// insane - 20 givens
+}
+
+// load how to play screen
+
+function displayGameRules() {}
+
+const sudokuGame = new SudokuGame();
